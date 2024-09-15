dcba6227ac42d726c345435ad25562ef47cccb42
krushnavardhan <62050470+krushnavardhan@users.noreply.github.com>
2024-09-15 17:27:30 +0530

    refs 15092024 Added Slf4j logging.

--- src/main/java/com/academy/controller/AdminDashboardController.java
+++ src/main/java/com/academy/controller/AdminDashboardController.java
@@ -1,19 +1,25 @@
 package com.academy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminDashboardController {
+	
+    private static final Logger log = LoggerFactory.getLogger(AdminDashboardController.class);
 
 	@RequestMapping("/")
 	public String getLoginPage() {
 		//Need to implement login page
+		log.info("Loading login page...");
 		return "redirect:/admin/dashboard";
 	}
 
 	@RequestMapping("/admin/dashboard")
 	public String getAdminDashboardPage() {
+		log.info("Loading admin dashboard page...");
 		return "adminDashboard";
 	}
 }
